a007d98c2e7b31998babf977c6bdcb7244ab3a5f
Jean Thomas <jeniferjeanthomas@gmail.com>
2020-10-21 16:24:16 -0400

    Moved all arrays to main, modified methods

--- GetToKnow/Program.cs
+++ GetToKnow/Program.cs
@@ -9,18 +9,54 @@ namespace GetToKnow
     {
         static void Main(string[] args)
         {
-            //due on Thursday @ 9
             string[] names = { "Caspian", "Sanjeev", "Daniel", "Ted", "Tevin", "Tobey", "Elisa", "Jermaine" };
             string[] surnames = { "Melendez", "Padilla", "Barrow", "Scott", "Lowery", "Reeve", "Mosley", "Mays" };
+            string[] hometowns = { "Detroit, MI", "Dearborn, MI", "Bloomfield Hills, MI", "Sterling Heights, MI", "Warren, MI", "Roseville, MI", "Eastpointe, MI", "Detroit, MI" };
+            string[] faveFoods = { "pizza", "ramen", "elote", "pesto", "cookies", "bulgogi", "burgers", "sushi" };
+            string[] birthdays = { "March 7th", "June 30th", "July 10th", "October 20th", "January 4th", "December 15th", "June 22nd", "April 3rd", };
 
             bool userContinue = true;
 
-            Console.WriteLine("Welcome to our C# class!");
+            Console.WriteLine("Welcome to our C# class! Here are our students:\n");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"{i+1}\t{names[i]}");
+            }
 
             while (userContinue)
             {
-                string input = GetUserInput($"Which student would you like to learn more about? Please enter a number between 1-{names.Length}.");
-                GetStudent(input, names, surnames);
+                string input = GetUserInput($"\nWhich student would you like to learn more about? Please enter a number between 1-{names.Length}.");
+
+                bool proceed = true;
+                
+                while (proceed)
+                {
+                    try
+                    {
+                        if (Int32.TryParse(input, out int studentNumber))
+                        {
+                            int index = studentNumber - 1;
+
+                            Console.Write($"Student {studentNumber} is {names[index]} {surnames[index]}. ");
+                            string moreInfo = GetUserInput($"What would you like to know about {names[index]}? (enter hometown, favorite food or birthday)");
+                            
+                            Console.Write(GetMoreInfo(index, moreInfo, names[index], hometowns, faveFoods, birthdays));
+                            proceed = UserContinue("Do you want to learn more about this student? (y/n)");
+                        }
+                        else
+                        {
+                            input = GetUserInput($"That student does not exist. Please try again. (enter a number 1-{names.Length})");
+                        }
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        input = GetUserInput($"That student does not exist. Please try again. (enter a number 1-{names.Length})");
+                    }
+                    catch (FormatException)
+                    {
+                        input = GetUserInput($"That student does not exist. Please try again. (enter a number 1-{names.Length})");
+                    }
+                }
                 userContinue = UserContinue("Do you want to learn about another student? (y/n)");
             }
             Console.WriteLine("Thanks!");
@@ -31,56 +67,21 @@ namespace GetToKnow
             string input = Console.ReadLine().Trim();
             return input;
         }
-        public static void GetStudent(string input, string[] names, string[] surnames)
+        public static string GetMoreInfo(int index, string input, string name, string[] info1, string[] info2, string[] info3)
         {
-            bool proceed = true;
-            while (proceed)
-            {
-                try
-                {
-                    if (Int32.TryParse(input, out int studentNumber))
-                    {
-                        int index = studentNumber - 1;
-                        Console.Write($"Student {studentNumber} is {names[index]} {surnames[index]}. ");
-                        string moreInfo = GetUserInput($"What would you like to know about {names[index]}? (enter hometown, favorite food or birthday)");
-                        Console.Write(GetMoreInfo(index, moreInfo, names[index]));
-                        proceed = UserContinue("Do you want to learn more about this student? (y/n)");
-                        continue;
-                    }
-                    else
-                    {
-                        input = GetUserInput($"That student does not exist. Please try again. (enter a number 1-{names.Length})");
-                    }
-                }
-                catch (IndexOutOfRangeException)
-                {
-                    input = GetUserInput($"That student does not exist. Please try again. (enter a number 1-{names.Length})");
-                }
-                catch (FormatException)
-                {
-                    input = GetUserInput($"That student does not exist. Please try again. (enter a number 1-{names.Length})");
-                }
-            }
-        }
-        public static string GetMoreInfo(int index, string input, string name)
-        {
-            string[] hometowns = { "Detroit, MI", "Dearborn, MI", "Bloomfield Hills, MI", "Sterling Heights, MI", "Warren, MI", "Roseville, MI", "Eastpointe, MI", "Detroit, MI" };
-            string[] faveFoods = { "pizza", "ramen", "elote", "pesto", "cookies", "bulgogi", "burgers", "sushi" };
-            string[] birthdays = { "March 7th", "June 30th", "July 10th", "October 20th", "January 4th", "December 15th", "June 22nd", "April 3rd", };
-
             try 
             { 
                 if (input == "hometown")
                 {
-                    return $"{name} is from {hometowns[index]}. ";
+                    return $"{name} is from {info1[index]}. ";
                 }
                 else if (input == "favorite food")
                 {
-                    return $"{name}'s favorite food is {faveFoods[index]}. ";
+                    return $"{name}'s favorite food is {info2[index]}. ";
                 }
                 else if (input == "birthday")
                 {
-                    return $"{name}'s birthday is {birthdays[index]}. ";
+                    return $"{name}'s birthday is {info3[index]}. ";
                 }
                 else
                     return "That data does not exist. ";
